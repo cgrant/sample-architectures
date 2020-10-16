3212c629df810a1bb06f1d3ffed452e41804aaa1
Christopher Grant <crgrant@gmail.com>
2020-10-16 17:13:25 -0500

    cleanup

--- simple/bootstrap.sh
+++ simple/bootstrap.sh
@@ -1,6 +1,12 @@
 
 PROJECT_ID=$(gcloud config get-value core/project)
 
+export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format 'value(projectNumber)')
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable containeranalysis.googleapis.com
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com --role roles/owner
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com --role roles/containeranalysis.admin
+
 
 # bootstrap.sh --destroy
 for arg in "$@"
